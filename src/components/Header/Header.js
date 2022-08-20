bd6a980f98e8b4c78ed68a33ab0fcd9090b17fb2
areezNU <76098491+areezNU@users.noreply.github.com>
2022-08-20 13:17:55 -0700

    Update Header.js
    height

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,23 +3,9 @@ import "./Header.css";
 import Cta from "./Cta";
 import ME from "../../assets/me1.png";
 import HeaderSocials from "./HeaderSocials";
-import { useEffect } from "react";
-import { useState } from "react";
 const Header = () => {
-  const [show, setShow] = useState(false);
-  const handleShow = () => {
-    if (window.innerWidth < 1024) {
-      setShow(true);
-    } else {
-      setShow(false);
-    }
-  };
-  // window.addEventListener("resize", handleShow);
-  useEffect(() => {
-    handleShow();
-  }, []);
   return (
-    <header id="Home" className={show ? "header_small" : "header"}>
+    <header id="Home" className="header">
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Areez Ali</h1>
